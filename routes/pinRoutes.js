4a51b601f66774f189c210e062f4d4537535d44a
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-07-26 16:13:05 -0700

    Update pinRoutes.js

--- routes/pinRoutes.js
+++ routes/pinRoutes.js
@@ -3,5 +3,10 @@ const pinCtrl = require("../controllers/pinCtrl");
 
 //herokuapp.placesga/pins/:id
 // loads pin page with all info INCLUDING users who have pinned this place with link to user page
+router.get("/", pinCtrl.homeMap);
+router.post("/", pinCtrl.createNewPin);
+router.put("/:id", pinCtrl.editPin);
+router.delete("/:id", pinCtrl.deletePin);
+router.get("/:id", pinCtrl.getOnePin);
 
 module.exports = router;
