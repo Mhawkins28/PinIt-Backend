a1da6798690213e649c378855d8c40e8aa825758
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-08-01 20:43:40 -0700

    Update pinCtrl.js

--- controllers/pinCtrl.js
+++ controllers/pinCtrl.js
@@ -3,79 +3,66 @@ const Pin = require("../models/Pin");
 
 const homeMap = (req, res) => {
   //load homepage map
-  Pin.find({}, (err, pins)=>{
-  if (err) {
-      res.status(400).json(err);
-      return;
-    }
-    res.json(pins);
-  });
-};
-
-
-
-const createNewPin = (req, res) => {
-  //create new pin
-  Pin.create(req.body, (err, newPin) => {
+  Pin.find({})
+    .populate("Owner")
+    .exec((err, pins) => {
       if (err) {
         res.status(400).json(err);
         return;
       }
-      // res.json(newPin);
-      // newPin.owner = req.user._id
-      Pin.save((err, newPin) => {
-        res.json(newPin);
-      });
-  });
-
+      res.json(pins);
+    });
 };
 
-
+const createNewPin = (req, res) => {
+  //create new pin
+  Pin.create(req.body, (err, newPin) => {
+    if (err) {
+      res.status(400).json(err);
+      return;
+    }
+    // res.json(newPin);
+    // newPin.owner = req.user._id
+    Pin.save((err, newPin) => {
+      res.json(newPin);
+    });
+  });
+};
 
 const editPin = (req, res) => {
-
   Pin.findByIdAndUpdate(req.params.id, req.body, (err, pin) => {
     if (err) {
-        res.status(400).json(err)
-        return
+      res.status(400).json(err);
+      return;
     }
 
     Pin.find({}, (error, pins) => {
-        res.json(pins)
-    })
-
-})
-
+      res.json(pins);
+    });
+  });
 };
-
-
 
 const deletePin = (req, res) => {
-  let {id} = req.params
+  let { id } = req.params;
 
   Pin.findByIdAndDelete(id, (err, deletedPin) => {
-      if (err) {
-          res.status(400).json(err)
-          return
-      }
-      
-      res.json(deletedPin)
-  })
+    if (err) {
+      res.status(400).json(err);
+      return;
+    }
+
+    res.json(deletedPin);
+  });
 };
 
-
-
-
 const getOnePin = (req, res) => {
-
   Pin.findById(req.params.id, (err, pin) => {
     if (err) {
-        res.status(400).json(err)
-        return
+      res.status(400).json(err);
+      return;
     }
-    res.json(pin)
-})
-
+    res.json(pin);
+  });
 };
 
 module.exports = {
